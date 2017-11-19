1a9fc93b3ce79d0faf41aad450c6048d5601988b
salman khan <khanproject97@gmail.com>
2017-11-19 13:44:31 +0545

    week6

--- week6csy2028/week6.php
+++ week6csy2028/week6.php
@@ -2,6 +2,7 @@
 
 <?php
 
+/*
 if(isset($_POST['save'])){
 
 	
@@ -30,6 +31,7 @@ if(isset($_POST['save'])){
 <input type ="text" name="num1"><br><br>
 
 <label>operator</label>
+<option value='+'>
 <input type ="text" name="operator"><br><br>
 
 <label>Enter second number </label>
@@ -38,3 +40,71 @@ if(isset($_POST['save'])){
 <input type="submit" name="save" value="submit">
 </form>
 
+*/
+
+if(isset($_POST['save'])){
+	
+
+	echo '<pre>' ;
+	print_r($_POST);
+
+	$fname=$_POST['fname'];
+	$lname=$_POST['lname'];
+	$email=$_POST['eaddress'];
+	$color=$_POST['favcolor'];
+	$address=$_POST['address'];
+	
+
+}
+
+
+
+
+?>
+
+<script 
+
+function validate(form){
+	
+	if(form.tandc.value=='yes'){
+
+	alert("Please accept terms and condition);
+	return false;
+	}
+	
+}
+
+</script>
+
+
+<form  method="POST" action="" onsubmit="return validate(this)">
+
+<label>Enter first name </label>
+<input type ="text" name="fname"><br><br>
+
+<label>enter last name</label>
+<input type ="text" name="lname"><br><br>
+
+<label>Enter email address </label>
+<input type ="text" name="eaddress"><br><br>
+
+<label>Favourite color<label>
+<select name="favcolor">
+
+<option value="red">RED</option>
+<option value="blue">BLUE</option>
+<option value="pink">PINK</option>
+<option value="green">GREEN</option>
+
+
+
+</select><br><br>
+<label>Address<label><br>
+<textarea name="address" rows="2" cols='8'></textarea><br><br>
+
+
+<label>Accept the Terms and condition </label>
+<input type="radio" name='tandc' value='yes'><br><br>
+
+<input type="submit" name="save" value="submit">
+</form>
